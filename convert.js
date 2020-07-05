0cd45dfbfa7ad17670df0a6ef3f8952f2ff1a8bd
Danny Hieber <dwhieb@gmail.com>
2020-07-05 17:22:24 -0500

    transliterate texts

--- convert.js
+++ convert.js
@@ -1,46 +1,85 @@
-import fs from 'fs';
-import dlx from '@digitallinguistics/transliterate';
+/**
+ * This script converts the raw, manually-entered transcriptions in the practical typing system
+ * to the original orthography used in the published versions of the texts
+ */
 
-const { transliterate } = dlx;
+/* eslint-disable
+  no-await-in-loop,
+*/
+
+import { createRequire } from 'module';
+import fs                from 'fs-extra';
+import { transliterate } from '@digitallinguistics/transliterate/transliterate.js';
+
+const require = createRequire(import.meta.url);
 
 const {
+  emptyDir,
   readdir: readDir,
   readFile,
   writeFile,
-} = fs.promises;
+} = fs;
 
-let subs = {};
+const substitutions = require(`./transliteration.json`);
 
-async function convertText(filename) {
+function convertText(text) {
 
-  let text       = await readFile(`./texts/raw/${filename}`, `utf8`);
-  const [header] = text.match(/---.+---/gsu);
-  text           = text.replace(/---.+---/gsu, ``);
-  text           = text.trim();
+  const newLineRegExp      = /(?:\r\n)+/gu;
+  const newUtteranceRegExp = /\r\n\s*\r\n/gu;
 
-  let utterances = text.split(/\n\s*\n/gu);
-  utterances     = utterances.map(u => u.split(/[\n\r]+/gu));
-  utterances     = utterances.map(u => u.filter(line => line));
-  utterances     = utterances.map(u => u.map(line => line.trim()));
-  utterances     = utterances.map(([num, trs, txn, ...lines]) => [num, transliterate(trs, subs), transliterate(txn, subs), ...lines]);
-  utterances     = utterances.map(u => u.join(`\r\n`));
-  text           = utterances.join(`\r\n\r\n`);
-  text           = `${header}\r\n\r\n\r\n${text}`;
-  await writeFile(`./texts/converted/${filename}`, text, `utf8`);
-  return text;
+  return text
+  .split(newUtteranceRegExp)
+  .map(u => u.split(newLineRegExp))
+  .map(u => u.filter(Boolean))
+  .map(u => u.map(line => line.trim()))
+  .map(([num, trs, txn, ...lines]) => {
 
-}
+    if (trs.startsWith(`\\trs`)) {
+      return [num, trs, txn, ...lines];
+    }
 
-async function convertTexts() {
+    return [
+      num,
+      transliterate(trs, substitutions),
+      transliterate(txn, substitutions),
+      ...lines,
+    ];
 
-  subs = await readFile(`./transliteration.json`, `utf8`);
-  subs = JSON.parse(subs);
-
-  const rawTextFilePaths = await readDir(`./texts/raw`);
-  const convertedTexts = await Promise.all(rawTextFilePaths.map(convertText));
-  const combinedTexts = convertedTexts.join(`\r\n`);
-  await writeFile(`./texts/converted/combined.txt`, combinedTexts);
+  })
+  .map(u => u.join(`\r\n`))
+  .join(`\r\n\r\n`);
 
 }
 
-convertTexts().catch(console.error);
+void async function convertFiles() {
+
+  await emptyDir(`texts/converted`);
+
+  const rawTextFilePaths = await readDir(`texts/raw`);
+  let   combinedConvertedTexts = ``;
+  let   combinedRawTexts       = ``;
+
+  for (const filename of rawTextFilePaths) {
+
+    const headerRegExp = /---.+---/gsu;
+    let   text         = await readFile(`texts/raw/${filename}`, `utf8`);
+
+    combinedRawTexts += text;
+
+    const [header] = text.match(headerRegExp);
+    text           = text.replace(header, ``);
+    text           = text.trim();
+    text           = convertText(text);
+    text           = `${header}\r\n${text}`;
+
+    await writeFile(`texts/converted/${filename}`, text, `utf8`);
+
+    combinedConvertedTexts += text;
+    combinedConvertedTexts += `\r\n`;
+
+  }
+
+  await writeFile(`texts/combined-converted.txt`, combinedConvertedTexts);
+  await writeFile(`texts/combined-raw.txt`, combinedRawTexts);
+
+}();
