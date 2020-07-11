b128b660d7fbcb045277cdef83f7df9d4075f09c
Danny Hieber <dwhieb@gmail.com>
2020-07-11 11:29:50 -0500

    add testRun flag to findAndReplace function

--- scripts/findAndReplace.js
+++ scripts/findAndReplace.js
@@ -10,9 +10,11 @@ const {
 /**
  * Searches the entire Nuuchahnulth corpus using the provided search function,
  * and saves any changes that are made to the corpus, overwriting old files
- * @param  {Function} searchFunction A function that accepts an utterance as an argument, and returns an updated utterance. Return the original utterance if no changes need to be made.
+ * @param {Function} searchFunction    A function that accepts an utterance as an argument, and returns an updated utterance. Return the original utterance if no changes need to be made.
+ * @param {Object}   [options={}]      An options hash
+ * @param {Boolean}  [options.testRun] Whether to resave the files over the originals (testRun = false), or as a new file (testRun = true). Defaults to true.
  */
-export default async function findAndReplace(searchFunction = u => u) {
+export default async function findAndReplace(searchFunction = u => u, { testRun = true } = {}) {
 
   const jsonPath  = path.join(`texts`, `json`);
   const filenames = await readDir(jsonPath);
@@ -21,15 +23,8 @@ export default async function findAndReplace(searchFunction = u => u) {
     const filePath = path.join(jsonPath, filename);
     const text = await readJSON(filePath);
     text.utterances = text.utterances.map(searchFunction);
-    await writeJSON(filePath, text, { spaces: 2 });
+    const writePath = testRun ? filePath.replace(`.json`, `-updated.json`) : filePath;
+    await writeJSON(writePath, text, { spaces: 2 });
   }));
 
-  // for each file
-  // - read the file, parse the JSON, get the utterances
-  // - apply searchFunction to each utterance using .map()
-  // - (save the new utterances array to the text)
-  // - write the JSON back to the same file
-
 }
-
-findAndReplace();
