1338c65a91b90c4001e1d5865d60ebd54e524d3c
Danny Hieber <dwhieb@gmail.com>
2020-07-11 11:57:28 -0500

    calculate corpus statistics

--- scripts/findAndReplace.js
+++ scripts/findAndReplace.js
@@ -1,5 +1,7 @@
-import fs   from 'fs-extra';
-import path from 'path';
+import createSpinner from 'ora';
+import fs            from 'fs-extra';
+import path          from 'path';
+import ProgressBar   from 'progress';
 
 const {
   readdir: readDir,
@@ -10,21 +12,38 @@ const {
 /**
  * Searches the entire Nuuchahnulth corpus using the provided search function,
  * and saves any changes that are made to the corpus, overwriting old files
- * @param {Function} searchFunction    A function that accepts an utterance as an argument, and returns an updated utterance. Return the original utterance if no changes need to be made.
- * @param {Object}   [options={}]      An options hash
- * @param {Boolean}  [options.testRun] Whether to resave the files over the originals (testRun = false), or as a new file (testRun = true). Defaults to true.
+ * @param {Function} searchFunction            A function that accepts an utterance as an argument, and returns an updated utterance. Return the original utterance if no changes need to be made.
+ * @param {Object}   [options={}]              An options hash
+ * @param {Boolean}  [options.testRun=true]    Whether to resave the files over the originals (testRun = false), or as a new file (testRun = true). Defaults to true.
+ * @param {Boolean}  [options.searchOnly=true] Whether function is being called for search, or for find and replace. If true, does not update/resave files. If false, files are saved/overwritten.
  */
-export default async function findAndReplace(searchFunction = u => u, { testRun = true } = {}) {
+export default async function findAndReplace(searchFunction = u => u, { searchOnly = true, testRun = true } = {}) {
+
+  const spinner = createSpinner(`Running ${searchOnly ? `search` : `find and replace`}${testRun ? ` as a test run` : ``}.`).start();
 
   const jsonPath  = path.join(`texts`, `json`);
   const filenames = await readDir(jsonPath);
 
+  const progressBar = new ProgressBar(`:bar :current :total :percent :eta`, { total: filenames.length });
+
   await Promise.all(filenames.map(async filename => {
+
     const filePath = path.join(jsonPath, filename);
-    const text = await readJSON(filePath);
+    const text     = await readJSON(filePath);
+
     text.utterances = text.utterances.map(searchFunction);
+
+    if (searchOnly) return progressBar.tick();
+
     const writePath = testRun ? filePath.replace(`.json`, `-updated.json`) : filePath;
     await writeJSON(writePath, text, { spaces: 2 });
-  }));
+    progressBar.tick();
+
+  })).catch(e => {
+    spinner.fail(e.message);
+    throw e;
+  });
+
+  spinner.succeed(`${searchOnly ? `Search` : `Find and replace`} complete.`);
 
 }
--- scripts/getCorpusStats.js
+++ scripts/getCorpusStats.js
@@ -1,6 +1,29 @@
-export default function getCorpusStats() {
-  // create a search function to pass to findAndReplace.js
-  // this search function will count the following (for now)
-  // - utterances
-  // - tokens
+import findAndReplace from './findAndReplace.js';
+
+async function getCorpusStats() {
+
+  const types    = new Set;
+  let utterances = 0;
+  let tokens     = 0;
+
+  await findAndReplace(({ words }) => {
+
+    utterances++;
+
+    if (words) {
+      tokens += words.length ?? 0;
+      words.forEach(({ analysis }) => types.add(analysis));
+    }
+
+  }, { searchOnly: true });
+
+  console.info(`Utterances: ${utterances}`);
+  console.info(`Tokens: ${tokens}`);
+  console.info(`Types: ${types.size}`);
+
 }
+
+getCorpusStats().catch(e => {
+  console.error(e);
+  throw e;
+});
